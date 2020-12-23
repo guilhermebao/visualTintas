1e6af353667831b0f112ed8d0503790579b48712
unknown <guilhermecarvalhobao@gmail.com>
2020-12-23 01:17:13 -0300

    Register

--- src/components/user/Register.vue
+++ src/components/user/Register.vue
@@ -1,60 +1,65 @@
 <template>
   <div class="register">
-    <PageTitle main="Cadastre-se" />
+    <div class="container-fluid input-grupo pt-5">
+    <PageTitle main="" />
+    <div class="externa pt-5">
+        <div class="line mb-4"></div>
+      </div>
+    <p class="p40 pb-5">Minha Conta</p>
     <div class="register-form form-group">
       <input
-        class="form-control"
+        class="form-control input__ mb-4"
         type="text"
         v-model="entity.cpfcnpj"
-        placeHolder="CPF ou CNPJ"
+        placeHolder="CPF"
       />
       <input
-        class="form-control"
+        class="form-control input__ mb-4"
         type="text"
         v-model="entity.name"
         placeHolder="Nome Completo ou Razão Social"
       />
       <input
-        class="form-control"
+        class="form-control input__ mb-4"
         type="text"
         v-model="entity.email"
         placeHolder="E-mail"
       />
       <input
-        class="form-control"
+        class="form-control input__ mb-4"
         type="text"
         v-model="entity.postalcode"
         placeHolder="CEP"
       />
       <input
-        class="form-control"
+        class="form-control input__ mb-4"
         type="text"
         v-model="entity.street"
         placeHolder="Rua"
       />
       <div class="form-row">
         <input
-          class="col form-control"
+          class="col form-control input__ mb-4"
           type="text"
           v-model="entity.number"
           placeHolder="Número"
         />
         <input
-          class="col form-control"
+          class="col form-control input__ mb-4"
           type="text"
           v-model="entity.complement"
           placeHolder="Complemento"
         />
       </div>
       <input
-        class="form-control"
+        class="form-control input__ mb-4"
         type="text"
         v-model="entity.neighborhood"
         placeHolder="Bairro"
       />
       <div class="form-row">
         <select
-          class="col form-control"
+          class="col form-control input__ mb-4"
           type="text"
           v-model="entity.state"
           placeHolder="Estado"
@@ -62,7 +67,7 @@
             <option value="" disabled selected>Estado</option>
         </select>
         <select
-          class="col form-control"
+          class="col form-control input__ mb-4"
           type="text"
           v-model="entity.city"
         >
@@ -70,7 +75,7 @@
         </select>
       </div>
       <input
-        class="form-control"
+        class="form-control input__ mb-4"
         type="text"
         v-model="entity.clientCode"
         placeHolder="Código do Cliente"
@@ -78,6 +83,7 @@
     </div>
     <div class="register-buttons">
         <button>Cadastrar</button>
+    </div>
     </div>
   </div>
 </template>
@@ -112,5 +118,16 @@ export default {
 .register-buttons {
     display: flex;
     justify-content: center;
+}
+
+.input__ {
+  border: 0;
+  border-bottom: 1px solid #CCCCCC;
+  border-radius: 0rem;
+}
+
+.input-grupo {
+  padding-left: 22rem;
+  padding-right: 22rem;
 }
 </style>
