5987a426df6d59486cc4ad3698af8f391e5e08e6
Mateus Ornelas <mateus.cler@hotmail.com>
2021-01-04 21:31:15 -0300

    home-items

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,18 +2,10 @@
   <div class="home">
     <div class="carrousel-area">
       <Carousel :perPage="1" :loop="true" :centerMode="true">
-        <Slide
-          ><img src="https://picsum.photos/1501/600" class="carousel-image" alt=""
-        /></Slide>
-        <Slide
-          ><img src="https://picsum.photos/1502/600" class="carousel-image" alt=""
-        /></Slide>
-        <Slide
-          ><img src="https://picsum.photos/1503/600" class="carousel-image" alt=""
-        /></Slide>
-        <Slide
-          ><img src="https://picsum.photos/1504/600" class="carousel-image" alt=""
-        /></Slide>
+        <Slide> <img src="https://picsum.photos/1501/600" class="carousel-image" alt=""/> </Slide>
+        <Slide> <img src="https://picsum.photos/1502/600" class="carousel-image" alt=""/> </Slide>
+        <Slide> <img src="https://picsum.photos/1503/600" class="carousel-image" alt=""/> </Slide>
+        <Slide> <img src="https://picsum.photos/1504/600" class="carousel-image" alt="" /> </Slide>
       </Carousel>
     </div>
     <div class="publicity-area">
@@ -21,6 +13,9 @@
       <span>Para facilitar suas compras Futuras. Adquira o valor que necessitar e nossa equipe de vendas irá gerar seu pedido conforme sua necessidade. Após efetuar o pagamento iremos gerar crédito em seu nome para você utilizar conforme sua necessidade.</span>
     </div>
     <button>Compre Vittin</button>
+
+    <PageTitle main="Produtos" />
+    
     <div class="product-area">
       
     </div>
@@ -29,10 +24,11 @@
 
 <script>
 import { Carousel, Slide } from "vue-carousel";
+import PageTitle from "../template/PageTitle";
 
 export default {
   name: "Home",
-  components: { Carousel, Slide },
+  components: { Carousel, Slide, PageTitle },
 
   data: function () {
     return {
---
+++ src/components/item/ItemCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-6 col-md-4">Formas de Pagamento</div>
+      <div class="col-6 col-md-4">Dúvidas Frequentes</div>
+      <div class="col-6 col-md-4">Fale Conosco</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
